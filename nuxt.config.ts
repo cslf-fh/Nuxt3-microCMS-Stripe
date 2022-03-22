19025a3e05ab40e03201df54b6ec17bb2df47b83
cslf-fh <consadole.sapporo.love.football@gmail.com>
2022-03-22 23:22:43 +0900

    fix config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 import { defineNuxtConfig } from 'nuxt3';
 
-const { MICROCMS_BASE_URL, MICROCMS_API_KEY, STRIPE_SECRET_KEY } = process.env;
+const { BASE_URL, MICROCMS_BASE_URL, MICROCMS_API_KEY, STRIPE_SECRET_KEY } =
+  process.env;
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
@@ -9,16 +10,13 @@ export default defineNuxtConfig({
     exposeConfig: true,
   },
   publicRuntimeConfig: {
-    baseUrl: 'http://localhost:3000',
+    baseUrl: BASE_URL,
     microcmsBaseUrl: MICROCMS_BASE_URL,
     microcmsApiKey: MICROCMS_API_KEY,
     stripeSecretKey: STRIPE_SECRET_KEY,
   },
   privateRuntimeConfig: {
-    baseUrl:
-      process.env.NODE_ENV !== 'production'
-        ? 'http://localhost:3000'
-        : undefined,
+    baseUrl: process.env.NODE_ENV !== 'production' ? BASE_URL : undefined,
     microcmsApiKey:
       process.env.NODE_ENV !== 'production' ? MICROCMS_API_KEY : undefined,
     stripeSecretKey:
